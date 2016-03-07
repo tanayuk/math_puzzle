ec662a41d9203177916421c7f10c01736510c3ed
Yuki Tanaka <tanaka@wedgebrothers.jp>
2016-03-07 09:03:22 +0900

    adding q03

---
+++ src/questions/q03.groovy
@@ -0,0 +1,15 @@
+package questions
+
+/**
+ * Created by tanayuk on 07/03/2016.
+ */
+def map = []
+(0..99).each{
+  map[it] = false
+}
+role = 2
+while(true){
+  for(int i = 0;i < 100;i++){
+
+  }
+}
