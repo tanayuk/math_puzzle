3a669f15ec881da3ac130ba107f74cbd841751f4
Yuki Tanaka <tanaka@wedgebrothers.jp>
2016-03-22 00:38:56 +0900

    Committing for more q24

---
+++ src/questions/q24.groovy
@@ -0,0 +1,14 @@
+package questions
+
+/**
+ * Created by tanayuk on 21/03/2016.
+ */
+
+while(true){
+  def count = []
+  def targets = (1..9)
+  def doubleHits = ['1':[2,4],'2':[2,3],'3':[2,6],'4':[1,7],'6':[3,9],'7':[4,8],'8':[7,9],'9':[8,6]]
+  def initial = 1
+
+
+}
