5fd1c680b9ca04c78e96a2a1c53831048ae4dd52
Yuki Tanaka <yuki_tanaka@r.recruit.co.jp>
2016-03-29 17:30:39 +0900

    Commit code iq 2769 and read me

--- src/codeiq/q2758.groovy
+++ src/codeiq/q2758.groovy
@@ -3,4 +3,9 @@ package codeiq
 /**
  * Created by tanayuk on 26/03/2016.
  */
+def input = "${System.in.newReader().readLine()}".split(" ")
+def n = Integer.valueOf(input[0])
+def m = Integer.valueOf(input[1])
+
+def field = new int[n+1][m+1]
 
