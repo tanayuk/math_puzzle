f98da3bff431c473d9e6cffe54cb1bbb79a5631b
Yuki Tanaka <yuki_tanaka@r.recruit.co.jp>
2016-04-01 19:26:46 +0900

    Adding math puzzle family

--- src/codeiq/q2769.groovy
+++ src/codeiq/q2769.groovy
@@ -3,3 +3,16 @@ package codeiq
 /**
  * Created by 01011996 on 3/29/16.
  */
+def input = "${System.in.newReader().readLine()}".split(" ")
+def n = Integer.valueOf(input[0])
+def m = Integer.valueOf(input[1])
+
+def field = new int[n+1][m+1]
+
+for(int i = 0; i <= n; i++){
+    for(int j = 0; j <=m; j++){
+
+
+
+    }
+}
