733e1d6cf84509e6c2739c37efd025a95ebd24c9
Yuki Tanaka <yuki_tanaka@r.recruit.co.jp>
2016-04-13 15:39:51 +0900

    Q08

---
+++ src/questions/q08.groovy
@@ -0,0 +1,16 @@
+package questions
+
+/**
+ * Created by 01011996 on 4/13/16.
+ */
+def addressList = []
+cleanUp(0, [])
+
+
+
+
+def cleanUp(int moveCount, addressList){
+  if(moveCount == 0){
+
+  }
+}
