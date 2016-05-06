fac394d1e0b2386393f6edf33737cb5bfd2f4631
Yuki Tanaka <yuki_tanaka@r.recruit.co.jp>
2016-05-06 19:43:35 +0900

    Committing final version of q2801

--- src/codeiq/q2801.groovy
+++ src/codeiq/q2801.groovy
@@ -13,34 +13,79 @@ package codeiq
  * Created by tanayuk on 4/19/16.
  */
 
-int[][][] bingo = new int[4][5][5]
+// Scanner
+Scanner cin=new Scanner(System.in)
 
-(0..3).each{ personNum ->
-  def input = "${System.in.newReader().readLine()}".trim().split(",")
+// Reading from stdin
+int[][][] bingo = new int[4][5][5]
+(0..3).each { personNum ->
+  def input = cin.nextLine().split(",")
   def count = 0
-  println("Person No: ${personNum}")
-  (0..4).each{ verticalNum->
-    (0..4).each{ horizontalNum->
+  (0..4).each { verticalNum ->
+    (0..4).each { horizontalNum ->
       bingo[personNum][verticalNum][horizontalNum] = Integer.valueOf(input[count])
       count++
     }
   }
 }
-
+// Answering
 println(calcMinimumNum(bingo))
 
-def calcMinimumNum(int[][][] bingo){
-  (0..11).each { pattern->
-    if(pattern < 5){
+def calcMinimumNum(int[][][] bingo) {
+  def min = 75
+  (0..11).each { pattern1 ->
+    (0..11).each { pattern2 ->
+      (0..11).each { pattern3 ->
+        (0..11).each { pattern4 ->
+          def aAndB = getTargetNumbers(getBingoNumbers(bingo[0],pattern1),getBingoNumbers(bingo[1], pattern2))
+          def cAndD = getTargetNumbers(getBingoNumbers(bingo[2], pattern3), getBingoNumbers(bingo[3], pattern4))
+          def targetNumbers = getTargetNumbers(aAndB,cAndD)
 
+          if(targetNumbers.size() <= min){
+            //println("Minimum is updated with ${targetNumbers.size()} with numbers: ${targetNumbers.sort()}")
+            min = targetNumbers.size()
+          }
+        }
+      }
     }
+  }
+  return min
+}
+
+/**
+ * Get bingo numbers based on pattern defined in Javadoc
+ *
+ * @param bingoBoard
+ * @param pattern
+ * @return bingo array
+ */
+def getBingoNumbers(int[][] bingoBoard, int pattern) {
+  if (pattern < 5) {
+    // horizontal bingo
+    return Arrays.asList(bingoBoard[pattern])
+  } else if (pattern < 10) {
+    // vertical bingo
+    return bingoBoard.collect { it[pattern - 5] }
+  } else if (pattern == 10) {
+    // left top to right bottom
+    return Arrays.asList(bingoBoard[0][0], bingoBoard[1][1], bingoBoard[2][2], bingoBoard[3][3], bingoBoard[4][4])
+  } else if (pattern == 11) {
+    // right top to left bottom
+    return Arrays.asList(bingoBoard[0][4], bingoBoard[1][3], bingoBoard[2][2], bingoBoard[3][1], bingoBoard[4][0])
+  } else {
+    return
   }
 }
 
-def getBingoNumbers(int[][] bingoBoard, int pattern){
-  if(pattern < 5){
-    return bingoBoard[pattern]
-  }else if(pattern < 10){
-    return bingoBoard[]
-  }
+/**
+ * Return intersection and differences of list
+ * @param x
+ * @param y
+ * @return list of numbers
+ */
+def getTargetNumbers(List x, List y) {
+  def commons = x.intersect(y)
+  def difference = x.plus(y)
+  difference.removeAll(commons)
+  return commons.plus(difference)
 }
